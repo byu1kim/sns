e63abea4a9b0ae9b80aff5fb1071b773279ca062
Byul Kim <caxbelle@gmail.com>
2023-03-07 02:32:15 -0800

    index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./Auth/AuthContext";
+import { AuthProvider } from "./authentication/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
